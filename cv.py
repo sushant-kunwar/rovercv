dfc04d97e13c17ab768d982e9584697c5eec6dc7
sushant-kunwar <112830659+sushant-kunwar@users.noreply.github.com>
2024-11-09 13:26:21 -0500

    Update cv.py
    implement yolov5

--- cv.py
+++ cv.py
@@ -1,1 +1,23 @@
 import cv2
+import torch
+
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to grab frame")
+        break
+
+    results = model(frame)
+
+    results.render() 
+
+    cv2.imshow('Webcam Live Feed with YOLOv5', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
